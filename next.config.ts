6f96ed7a22ce254507154a305a65eaaceb9d35d5
kiritocode1 <kathawalearyan9@gmail.com>
2025-11-23 11:23:24 +0530

    Child welfare commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["onnxruntime-node"],
+  outputFileTracingIncludes: {
+    "/api/tts": ["./model/**/*"],
+  },
 };
 
 export default nextConfig;
